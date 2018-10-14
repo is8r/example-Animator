4644ff69d0de12dc040fbe40c6e7c736b2295afc
Yu Ishihara <ishihara.yu@gmail.com>
2018-10-14 16:53:40 +0900

    Add files

---
+++ Assets/Lama/Lama.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lama : MonoBehaviour
+{
+
+    //アニメーターのコンポーネントを入れる変数を宣言する
+    Animator animator;
+
+    void Start()
+    {
+        //アニメーターのコンポーネントを取得する
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        //ボタンの操作を検知したら
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+            //アニメーターのトリガーに引数を渡す
+            animator.SetTrigger("Up");
+        }
+
+        //その他の方向も同様に設定する
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            animator.SetTrigger("Right");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            animator.SetTrigger("Down");
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            animator.SetTrigger("Left");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+### Animatorでアニメーションさせるサンプル
+
+![](images/0.gif)
+
+使用させていただいた素材:
+
+- ドット絵世界―キャラ素材1 http://yms.main.jp/dotartworld/page3/chara01.html
